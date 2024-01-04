583d0b3f72c68eb204bef08e4128fbf9527c06c6
rachmi00 <r.agyingi@gmail.com>
2024-01-04 18:29:12 +0100

    some changes

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,19 @@
-import { Outlet,  useLoaderData, Link , Form, } from 'react-router-dom'
+import {
+  Outlet,
+  useLoaderData,
+  Link,
+  NavLink,
+  Form,
+  redirect,
+  useNavigation
+} from 'react-router-dom'
 import { getContacts, createContact } from "../Contacts";
 
 export async function action() {
-    const contact = await createContact();
-    return { contact };
-  }
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+
+}
 
 export async function loader() {
   const contacts = await getContacts();
@@ -12,39 +21,52 @@ export async function loader() {
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
+  const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
           {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
+                    <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
+                    {/* other code */}
+                  </NavLink>
                   <Link to={`contacts/${contact.id}`}>
                     {contact.first || contact.last ? (
                       <>
@@ -63,11 +85,16 @@ export default function Root() {
               <i>No contacts</i>
             </p>
           )}
-          </nav>
-        </div>
-        <div id="detail">
-            <Outlet/>
-        </div>
-      </>
-    );
-  }
+        </nav>
+      </div>
+      <div id="detail"
+         className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
+      
+        <Outlet />
+      </div>
+    </>
+  );
+}
